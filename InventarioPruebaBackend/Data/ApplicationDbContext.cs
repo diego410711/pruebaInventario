ed4668db3b40e104f6a250693adbab15e66645d4
diego41071 <alexander.diego.2007@hotmail.com>
2025-03-18 17:45:27 -0500

    primer commit

---
+++ InventarioPruebaBackend/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using InventarioPrueba.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventarioPrueba.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
